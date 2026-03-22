db0645f62e324e20530bcf0ab85897d8ed807a0e
abwalayhm18-maker <abwalayhm18@gmail.com>
2026-03-22 20:09:41 +0300

    Change Firebase config to new project settings
    Updated Firebase configuration with new project details.

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,14 +5,13 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJOaDkXvm5XP3-Fu0LsuKvQ0_dZK5uf-k",
-  authDomain: "school-77936.firebaseapp.com",
-  databaseURL: "https://school-77936-default-rtdb.firebaseio.com",
-  projectId: "school-77936",
-  storageBucket: "school-77936.firebasestorage.app",
-  messagingSenderId: "783345000886",
-  appId: "1:783345000886:web:585c7579926bcb8f0ca117",
-  measurementId: "G-XX9L099J8H"
+  apiKey: "AIzaSyB0TuG4KnRDj7a1xIcgChhuJtfaf8nnZl4",
+  authDomain: "newqwe-b03a2.firebaseapp.com",
+  projectId: "newqwe-b03a2",
+  storageBucket: "newqwe-b03a2.firebasestorage.app",
+  messagingSenderId: "276023721661",
+  appId: "1:276023721661:web:07f6dd3e2d3e23b2f9c1d4",
+  measurementId: "G-8EML9GT4W0"
 }
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
